1e7dbd91d25802696f3baa03d769d7efa1a03dc1
admin <admin@OSMARVM0>
2017-11-18 11:58:54 -0200

    Comentários

--- src/Principal.java
+++ src/Principal.java
@@ -19,10 +19,30 @@
  */
 public class Principal {
 
-    /**
-     * Seleciona um numero aleatorio no intervalo de inicio a fim.
+     /**
+     * O piso (= floor) de um número real x é o resultado do arredondamento de x
+     * para baixo. Em outras palavras, o piso de x é o único número inteiro i
+     * tal que i<=x<i+1. Ex. O piso de 3.9 é 3
      *
-     * @param inicio Inicio do intervalo do número aleatório
+     * Em java pode ser utilizando Math.floor(double)
+     *
+     * @param x Numero real a ser cálculado o piso.
+     * @return um valor inteiro com o piso de x.
+     */
+    public static int piso(double x) {
+        //Pego a parte inteira de x
+        int parteInteira = (int) x;
+        //Pego a parte fracionária de x
+        double parteFracionaria = x - parteInteira;
+        //Retorno x subtraindo a parte fracionaria 
+        return (int) (x - parteFracionaria);
+    }    
+    
+    
+    /**
+     * Seleciona um numero aleatorio no intervalo de início a fim.
+     *
+     * @param inicio Início do intervalo do número aleatório
      * @param fim Fim do intervalo do número aleatório
      */
     public static int aleatorio(int inicio, int fim) {
@@ -43,7 +63,7 @@ public class Principal {
     }
 
     /**
-     * Particione encontra o pivo.
+     * Particione encontra o pivô.
      *
      * Complexidade de tempo Theta(n).
      * T(n) = Theta(2n + 4) + O(2n) = Theta(n).
@@ -57,27 +77,27 @@ public class Principal {
      */
     public static int particione(int A[], int p, int r) {
         //x é o "pivô"
-        int x = A[r - 1];                     //Theta(1)
-        int i = p - 1;                      //Theta(1)
-        for (int j = p; j <= r - 1; j++) {  //Theta(n)
-            if (A[j - 1] <= x) {              //Theta(n)
-                i = i + 1;                  //O(n)
-                troca(A, i, j);             //O(n)
+        int x = A[r - 1];                       //Theta(1)
+        int i = p - 1;                          //Theta(1)
+        for (int j = p; j <= r - 1; j++) {      //Theta(n)
+            if (A[j - 1] <= x) {                //Theta(n)
+                i = i + 1;                      //O(n)
+                troca(A, i, j);                 //O(n)
             }
         }
-        troca(A, i + 1, r);                 //Theta(1)
-        return i + 1;                       //Theta(1)
+        troca(A, i + 1, r);                     //Theta(1)
+        return i + 1;                           //Theta(1)
     }
 
     /**
      * Particione aleatório encontra o pivo de forma aleatória.
      *
-     * Slide 91 da aula 01/09/2017 .
+     * Slide 91 da aula 01/09/2017.
      *
      * @param A Vetor com os dados
      * @param p Início do vetor
      * @param r Fim do vetor
-     * @return o pivo da troca
+     * @return o pivô da troca
      */
     public static int particioneAleatorio(int A[], int p, int r) {
         //i é o "pivô"
@@ -88,7 +108,10 @@ public class Principal {
 
     /**
      * Recebe um vetor A[1...n] e devolve o valor do i-ésimo menor elemento de
-     * A. A complexidade de tempo no pior caso n = r - p + 1 T(n) = max{T(k-1),
+     * A. 
+     * 
+     * A complexidade de tempo no pior caso n = r - p + 1 
+     * T(n) = max{T(k-1),
      * T(n-k)} + Theta(n) T(n) = Theta(n^2)
      *
      * A complexidade para o caso médio é Theta(n)
@@ -127,29 +150,43 @@ public class Principal {
      * @return A posição da mediana em A
      */
     private static int medianaN(int A[], int p, int r) {
-        int n = r;
-        int m = n / 2;
-        selectAleatorio(A, p, n, m); //Particiona todo o vetor coloca a media na posição m
+        //Calcula a quantidade de elementos de p até r
+        //p = 1 e r = 5 = 5 - 1 + 1 = 5 elementos
+        //p = 6 e r = 10 = 10 - 6 + 1 = 5 elementos
+        int n = r - p + 1;
+        //Calcula a posição da mediana em relação a quantidade de elementos do intervalo de p até r
+        int m =  piso(n/2);
+        selectAleatorio(A, p, r, m); //Particiona todo o vetor coloca a media na posição m        
         if (n % 2 == 1) {
+            //Quantidade ímpar 
+            //Início do vetor mais a mediana
+            //p = 6 e r = 10 = 10 - 6 + 1 = 5 elementos
+            //m = piso(5 / 2) = 2
+            //mediana = 6 + 2 = 8
             return (p + m);
         } else {
-            //Particiona todo o vetor para encontrar a mediana
-            return ((p + m) + (p + m - 1)) / 2;
-        }
+            //Quantidade par 
+            //Início do vetor mais a mediana
+            //p = 5 e r = 10 = 10 - 5 + 1= 6 elementos
+            //m = piso(6 / 2) = 3
+            //mediana = 6 + 3 - 1 = 7
+            return (p + m - 1);
+        }  
     }
 
     public static void main(String args[]) {
 
         //Vetor de dados
-        int A[] = {99, 33, 55, 77, 11, 22, 88, 66, 44};
+        int A[] = {99, 33, 55, 77, 11, 22, 88, 66, 44}; //Qtde ímpar de elementos
+        //int A[] = {99, 33, 55, 77, 11, 22, 88, 66}; //Qtde par de elementos
         
         //Quantidade de elementos
         int r = A.length;
 
         System.out.println(">>> Cálculo da Mediana com complexidade linear<<<");
         System.out.println("Vetor A antes: ");
-        for (int i = 0; i < r; i++) {
-            System.out.println((i + 1) + " - " + A[i]);
+        for (int i = 1; i <= r; i++) {
+            System.out.println((i) + " - " + A[i-1]);
         }
 
         //Localiza a mediana
@@ -159,8 +196,8 @@ public class Principal {
         System.out.println("O Valor é da Mediana: " + A[q - 1]);
 
         System.out.println("Vetor A após: ");
-        for (int i = 0; i < r; i++) {
-            System.out.println((i + 1) + " - " + A[i]);
+        for (int i = 1; i <= r; i++) {
+            System.out.println(i + " - " + A[i-1]);
         }
     }
 }
